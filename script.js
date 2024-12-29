704ebb575770a1bad1a84692644d913002c70cd1
Victor Ulloa <victoralfonsoulloa@gmail.com>
2024-12-29 01:17:12 -0700

    Add different uitility functions to hide and show loading window, as well as puzzle window

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const pokemonImageElement = document.getElementById("pokemonImage");
 const optionsContainer = document.getElementById("options");
 const pointsElement = document.getElementById("pointsValue");
 const totalCount = document.getElementById("totalCount");
-const mainContainer = document.getElementById("container");
+const mainContainer = document.getElementsByClassName("container");
 const loadingContainer = document.getElementById("loadingContainer");
 
 // Initialize variables 
 let usedPokemonIds = [];
 let count = 0;
 let points = 0;
+let showLoading = false;
 
 // Create function to fetch one Pokemon with an ID
 async function fetchPokemonById(id) {
+    showLoading = true;
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await response.json();
     return data;
@@ -30,6 +32,11 @@ async function fetchPokemonById(id) {
 
 // Function to load question with options
 async function loadQuestionWithOptions() {
+    if (showLoading) {
+        showLoadingWindow();
+        hidePuzzleWindow();
+    }
+
     // Fetch correct answer first
     let pokemonId = getRamdonPokemonId();
 
@@ -64,6 +71,11 @@ async function loadQuestionWithOptions() {
 
         console.log(options);
         console.log(optionsIds)
+
+        // Turn off loading if all options have been fetched.
+        if (options.length === 4) {
+            showLoading = false;
+        }
     }
 
     shuffleArray(options);
@@ -79,7 +91,12 @@ async function loadQuestionWithOptions() {
         button.textContent = option;
         button.onclick = (event) => checkAnswer(option === pokemon.name, event);
         optionsContainer.appendChild(button);
-    })
+    });
+
+    if (!showLoading) {
+        hideLoadingWindow();
+        showPuzzleWindow();
+    }
 }
 
 // Create checkAnswer function 
@@ -108,7 +125,12 @@ function checkAnswer(isCorrect, event) {
         event.target.classList.add("wrong");
     }
 
-    
+    // Load the next question with a 1s delay for the user to read the result
+
+    setTimeout(() => {
+        showLoading = true;
+        loadQuestionWithOptions();
+    }, 1000)
 }
 
 // Initial Load
@@ -131,3 +153,26 @@ function displayResult(result) {
     resultElement.textContent = result;
 }
 
+// Hide loading 
+function hideLoadingWindow() {
+    loadingContainer.classList.add("hide");
+}
+
+function showLoadingWindow() {
+    mainContainer[0].classList.remove("show");
+    loadingContainer.classList.remove("hide");
+    loadingContainer.classList.add("show");
+}
+
+// Show puzzle window
+function showPuzzleWindow() {
+    loadingContainer.classList.remove("show");
+    mainContainer[0].classList.remove("hide");
+    mainContainer[0].classList.add("show");
+}
+
+
+// Hide puzzle window
+function hidePuzzleWindow() {
+    mainContainer[0].classList.add("hide");
+}
